941d580573a9719d1005904489f4e9885baa49a0
tommypearce <tommmypearce52@gmail.com>
2024-02-05 19:30:43 -0500

    Added first HTML page with Hello World message.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Robert Smith! I am adding my first code change.'
+    return 'Hello World from Thomas Pearce! I am adding my first code change.'
 
 @app.route('/hello')
 def hello():
